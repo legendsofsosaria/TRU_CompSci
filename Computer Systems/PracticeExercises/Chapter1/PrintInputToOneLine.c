09ca4e724a87e0a04c2767d99cfd25fa0a9605bd
Elizabeth House <52378763+legendsofsosaria@users.noreply.github.com>
2024-05-27 11:49:29 -0300

    Too many exercises, going to have to restructure again

---
+++ Computer Systems/PracticeExercises/Chapter1/PrintInputToOneLine.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Write a program that prints its input one word per line */
+
+main()
+{
+
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+            putchar('\n');
+        else
+            putchar(c);
+    }
+    
+    return 0;
+}
--- Computer Systems/PracticeExercises/EvenOrOdd.c
+++ Computer Systems/PracticeExercises/EvenOrOdd.c
@@ -14,13 +14,14 @@ void isEvenOrOdd(int number)
         printf("%d is an even number.\n", number);
     }
 }
-
 int main() 
 {
     int number;
 
     /* Input number from user */
     printf("Enter an integer: ");
+
+    /* I realize scanf isn't discussed until chapter 7, however I could not figure out how to do this with what we did cover. */
     scanf("%d", &number);
 
     /* Check if the number is even or odd */
