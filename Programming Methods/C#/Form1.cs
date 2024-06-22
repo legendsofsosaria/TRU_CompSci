723a9b4c73b5b323fb1384f32cdb9d7321cd4c58
Elizabeth House <52378763+legendsofsosaria@users.noreply.github.com>
2024-06-22 17:00:29 -0300

    Initial C# project commit

---
+++ Programming Methods/C#/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_First_Project
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void outputLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void nameTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void readInputButton_Click(object sender, EventArgs e)
+		{
+			outputLabel.Text = nameTextBox.Text;
+		}
+
+		private void exitButton_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
